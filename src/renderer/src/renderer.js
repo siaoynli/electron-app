b2f5ac07211539edabd353cdfe6c04d4e9163290
siaoynli <siaoynli@126.com>
2024-06-26 21:17:54 +0800

    ok

---
+++ src/renderer/src/renderer.js
@@ -0,0 +1,31 @@
+function init() {
+  window.addEventListener('DOMContentLoaded', () => {
+    doAThing()
+  })
+}
+
+function doAThing() {
+  const versions = window.electron.process.versions
+  replaceText('.electron-version', `Electron v${versions.electron}`)
+  replaceText('.chrome-version', `Chromium v${versions.chrome}`)
+  replaceText('.node-version', `Node v${versions.node}`)
+
+  const ipcHandlerBtn = document.getElementById('ipcHandler')
+  ipcHandlerBtn?.addEventListener('click', () => {
+    window.electron.ipcRenderer.send('ping')
+  })
+
+  const updateHandlerBtn = document.getElementById('updateHandler')
+  updateHandlerBtn?.addEventListener('click', () => {
+    window.electron.ipcRenderer.send('update')
+  })
+}
+
+function replaceText(selector, text) {
+  const element = document.querySelector(selector)
+  if (element) {
+    element.innerText = text
+  }
+}
+
+init()
