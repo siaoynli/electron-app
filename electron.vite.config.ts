2a5bd16378d5c981990e62bf209d561a5067a985
siaoynli <siaoynli@126.com>
2024-07-02 16:33:52 +0800

    配置代理

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -16,17 +16,17 @@ export default defineConfig({
         '@views': resolve('src/renderer/src/views'),
         '@router': resolve('src/renderer/src/router'),
         '@store': resolve('src/renderer/src/store'),
-        '@components': resolve('src/renderer/src/components'),
+        '@components': resolve('src/renderer/src/components')
       }
     },
     server: {
-      "proxy": {
-        "/api": {
-          "target": "http://uat.crm.xuexiluxian.cn",
-          "changeOrigin": true,
-          "secure": false,
-          "rewrite": path => path.replace(/^\/api/, '')
-        },
+      proxy: {
+        '/api': {
+          target: 'http://uat.crm.xuexiluxian.cn',
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
       }
     },
     plugins: [vue()]
